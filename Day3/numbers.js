5854ffe32225dc420182f2783cbe8a1a07fcd227
kalai0408 <62752977+kalai0408@users.noreply.github.com>
2026-04-26 17:47:09 +0530

    Add function to classify numbers as positive, negative, or neutral
    Numbers

---
+++ Day3/numbers.js
@@ -0,0 +1,18 @@
+
+function number(n)
+{
+    let num = n;
+    if(num>0)
+    {
+       console.log("The number is postive")
+    }
+    else if (num<0)
+    {
+        console.log("The number is negative")
+    }
+    else
+    {
+        console.log("The number is neutral")
+    }
+}
+number(-2)
