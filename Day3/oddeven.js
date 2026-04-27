106312031bb4cd0ca04c732e0b1f5e82d04e2170
kalai0408 <62752977+kalai0408@users.noreply.github.com>
2026-04-27 09:20:07 +0530

    Add files via upload
    Odd or even number check

---
+++ Day3/oddeven.js
@@ -0,0 +1,15 @@
+
+function oddeven(num)
+{
+    let number = num
+    if (num%2==0)
+    {
+       console.log( "Even number")
+    }
+    else
+    {
+        console.log(" Odd number")
+    }
+}
+
+oddeven(5)
